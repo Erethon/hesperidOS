65bd998e2a35a00408938d3ed7dc11363d3f38e9
Dionysis Grigoropoulos <dgrig@erethon.com>
2024-03-25 23:44:52 +0200

    Init repo with a simple flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Erethon's (dgrig) NixOS setup";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    impermanence.url = "github:nix-community/impermanence";
+  };
+
+  outputs = {self, nixpkgs, impermanence, ...}@inptus: {
+    formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt;
+    nixosConfigurations.niato = nixpkgs.lib.nixosSystem{
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/niato/configuration.nix
+        impermanence.nixosModules.impermanence
+      ];
+    };
+  };
+}
