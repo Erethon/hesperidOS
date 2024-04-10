578085b828595b2e9fe8ad66b8c8f1d7cdfaab49
Dionysis Grigoropoulos <dgrig@erethon.com>
2024-04-11 01:07:22 +0300

    emacs: Init module with all required packages
    My configuration for emacs still resides in my dotfiles repo

---
+++ modules/emacs/default.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs;
+    [
+      ((emacsPackagesFor pkgs.emacs29-nox).emacsWithPackages (epkgs: [
+        epkgs.evil
+        epkgs.evil-collection
+        epkgs.evil-leader
+        epkgs.monokai-theme
+        epkgs.org-journal
+        epkgs.projectile
+      ]))
+    ];
+}
