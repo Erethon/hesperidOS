030c94a591e5efab9007c772015e52e65ec36db1
Dionysis Grigoropoulos <dgrig@erethon.com>
2024-05-08 23:04:15 +0300

    default: Add SSH keys to user dgrig by default

--- default.nix
+++ default.nix
@@ -30,6 +30,10 @@
     isNormalUser = true;
     extraGroups = [ "wheel" ];
     initialPassword = "vmonlypass";
+    openssh.authorizedKeys.keys = [
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIPb9z1U7Sti2lls0mlcmyPwmwD91amKwVlLZHYclSoULAAAABHNzaDo="
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBg4C7jOGuVMxSvUlGaZXf0JD/jag//1kFl5okKhjQhF"
+    ];
   };
   users.defaultUserShell = pkgs.zsh;
 
