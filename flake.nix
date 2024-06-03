d07e7cbe38e5f2bd1385f46a98193ef5cf9472c8
Dionysis Grigoropoulos <dgrig@erethon.com>
2024-06-03 17:08:06 +0300

    flake: Update sto 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Erethon's (dgrig) NixOS setup";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     impermanence.url = "github:nix-community/impermanence";
   };
 
