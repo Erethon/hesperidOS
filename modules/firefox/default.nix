09d02ec42d2d05c6252187cf1b97d4e87448a61e
Dionysis Grigoropoulos <dgrig@erethon.com>
2024-07-23 00:45:18 +0300

    firefox: Install extensions and lockdown profile

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -7,6 +7,58 @@
       DisablePocket = true;
       DisableFirefoxAccounts = true;
       DisableFirefoxScreenshots = true;
+      DisplayBookmarksToolbar = "never";
+      TranslateEnabled = false;
+      PromptForDownloadLocation = true;
+      OfferToSaveLogins = false;
+      OverrideFirstRunPage = "";
+      NetworkPrediction = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+        EmailTracking = true;
+      };
+      ExtensionSettings = {
+        # Prevent installation of extensions manually
+        "*".installation_mode = "blocked";
+        "uBlock0@raymondhill.net" = {
+          install_url =
+            "https://addons.mozilla.org/firefox/downloads/file/4290466/ublock_origin-1.58.0.xpi";
+          installation_mode = "force_installed";
+        };
+        "tridactyl.vim@cmcaine.co.uk" = {
+          install_url =
+            "https://addons.mozilla.org/firefox/downloads/file/4261352/tridactyl_vim-1.24.1.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+      Preferences = {
+        "geo.enabled" = {
+          Value = false;
+          Status = "locked";
+        };
+        "media.peerconnection.enabled" = {
+          Value = false;
+          Status = "locked";
+        };
+        "privacy.donottrackheader.enabled" = {
+          Value = true;
+          Status = "locked";
+        };
+      };
+      SanitizeOnShutdown = {
+        "Cache" = true;
+        "Cookies" = true;
+        "Downloads" = true;
+        "FormData" = true;
+        "History" = false;
+        "Sessions" = false;
+        "SiteSettings" = false;
+        "OfflineApps" = true;
+        "Locked" = true;
+      };
     };
   };
 }
