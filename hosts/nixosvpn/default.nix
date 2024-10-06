85d03f103ab10b6746b0c029be5f523f00da1b42
Dionysis Grigoropoulos <dgrig@erethon.com>
2024-10-06 22:37:15 +0300

    nixosvpn: Hardcode network config

--- hosts/nixosvpn/default.nix
+++ hosts/nixosvpn/default.nix
@@ -1,4 +1,6 @@
-{ config, lib, pkgs, ... }: {
+{ config, lib, pkgs, ... }:
+let hostip = "192.168.135.10";
+in {
   imports = [ ./hardware-configuration.nix ];
   disabledModules = [ "services/networking/headscale.nix" ];
   boot.loader.grub.enable = true;
@@ -12,8 +14,9 @@
   nix.settings.trusted-users = [ "root" "dgrig" ];
   services.headscale = {
     enable = true;
-    address = "192.168.135.10";
+    address = hostip;
     settings.server_url = "https://hs.erethon.com";
+    settings.metrics_listen_addr = "${hostip}:9099";
     settings.dns.base_domain = "ts.erethon";
     #settings.acl_policy_path = ./acl.json;
     settings.derp.server = {
@@ -28,8 +31,18 @@
   };
 
   networking.firewall = {
-    allowedTCPPorts = [ 8080 ];
+    allowedTCPPorts = [ 8080 9099 ];
     allowedUDPPorts = [ 3478 ];
+  };
+  networking.interfaces.ens3 = {
+    ipv4.addresses = [{
+      address = hostip;
+      prefixLength = 24;
+    }];
+  };
+  networking.defaultGateway = {
+    address = "192.168.135.1";
+    interface = "ens3";
   };
 
   system.stateVersion = "24.05"; # DO NOT CHANGE ME
