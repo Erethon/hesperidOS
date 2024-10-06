3a9e81ba919577647ae23815f8b7bea30981185d
Dionysis Grigoropoulos <dgrig@erethon.com>
2024-10-06 22:27:38 +0300

    unbound: Add support for quering tailscale hosts

--- modules/unbound/default.nix
+++ modules/unbound/default.nix
@@ -1,13 +1,30 @@
-{ ... }: {
-  services = {
-    unbound = {
+{ config, lib, ... }: {
+  options.unbound = {
+    tsdomain = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+      description = "Tailscale domain to configure";
+    };
+  };
+  config = {
+    services.unbound = {
       enable = true;
-      settings = {
+      settings = let
+        domain =
+          lib.mkIf (config.unbound.tsdomain != "") config.unbound.tsdomain;
+      in {
+        domain-insecure = domain;
         server = { interface = [ "127.0.0.1" ]; };
-        forward-zone = [{
-          name = ".";
-          forward-addr = "1.1.1.1";
-        }];
+        forward-zone = [
+          (lib.mkIf (config.unbound.tsdomain != "") {
+            name = config.unbound.tsdomain;
+            forward-addr = "100.100.100.100";
+          })
+          {
+            name = ".";
+            forward-addr = "1.1.1.1";
+          }
+        ];
       };
     };
   };
