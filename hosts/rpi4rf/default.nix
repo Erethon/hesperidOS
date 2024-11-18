517cfbb3bfb208ba69a178abddc8c3b902aa4e19
Dionysis Grigoropoulos <dgrig@erethon.com>
2024-11-19 01:26:32 +0200

    rpi4rf: Init RPI RF host

---
+++ hosts/rpi4rf/default.nix
@@ -0,0 +1,17 @@
+{ config, lib, pkgs, ... }: {
+
+  networking.networkmanager.enable = false;
+  networking.interfaces."wlan0".useDHCP = true;
+  networking.wireless = {
+    enable = true;
+    interfaces = [ "wlan0" ];
+    networks."fixme".psk = "fixme"; # TODO Update with proper sops-nix secrets
+    extraConfig = "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=wheel";
+  };
+  networking.hostName = "rpi4rf";
+  time.timeZone = "Europe/Athens";
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "24.05"; # DO NOT CHANGE ME
+}
