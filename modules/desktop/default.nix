10befbbac4e84ed3bc995b580656a96b13c10ccc
Dionysis Grigoropoulos <dgrig@erethon.com>
2025-02-11 16:25:56 +0200

    desktop: lint via statix

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -11,21 +11,35 @@ let
   );
 in
 {
-  services.xserver = {
-    enable = true;
-    xkb.layout = "us,gr,ru";
-    xkb.variant = ",,phonetic";
-    xkb.options = "grp:alt_shift_toggle,ctrl:nocaps";
-    #libinput.enable = true;
-    windowManager.dwm.enable = true;
-    windowManager.dwm.package = pkgs.dwm.override { conf = ../../patches/dwm-config.h; };
+  services = {
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "us,gr,ru";
+        variant = ",,phonetic";
+        options = "grp:alt_shift_toggle,ctrl:nocaps";
+      };
+      windowManager = {
+        dwm.enable = true;
+        dwm.package = pkgs.dwm.override { conf = ../../patches/dwm-config.h; };
+      };
+    };
+
+    udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="amdgpu_bl0", MODE="0666", RUN+="${pkgs.coreutils}/bin/chmod a+w /sys/class/backlight/%k/brightness"
+      KERNEL=="uhid", SUBSYSTEM=="misc", GROUP="tss", MODE="0660"
+    '';
+
+    pipewire.extraConfig.pipewire."00-disable-bell" = {
+      "context.properties" = {
+        "module.x11.bell" = "false";
+      };
+    };
   };
   fonts.packages = [ custom_terminus ];
 
-  services.opensnitch.enable = true;
   environment.systemPackages = with pkgs; [
     age
-    opensnitch-ui
     neomutt
     isync
     msmtp
@@ -48,17 +62,4 @@ in
     whois
   ];
   programs.ssh.startAgent = true;
-  users.users.dgrig.extraGroups = [
-    "wheel"
-    "video"
-  ];
-  services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="amdgpu_bl0", MODE="0666", RUN+="${pkgs.coreutils}/bin/chmod a+w /sys/class/backlight/%k/brightness"
-  '';
-
-  services.pipewire.extraConfig.pipewire."00-disable-bell" = {
-    "context.properties" = {
-      "module.x11.bell" = "false";
-    };
-  };
 }
