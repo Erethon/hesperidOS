15d5406f1ebc7ca65c98af1ca7ba4022384251ee
Dionysis Grigoropoulos <dgrig@erethon.com>
2026-05-09 12:20:19 +0300

    unbound: Update module to add home domain

--- modules/unbound/default.nix
+++ modules/unbound/default.nix
@@ -1,40 +1,71 @@
 { config, lib, ... }:
 {
   options.unbound = {
-    tsdomain = lib.mkOption {
+    tsDomain = lib.mkOption {
       type = lib.types.str;
       default = "";
       description = "Tailscale domain to configure";
     };
-    listenAddress = lib.mkOption {
+
+    tsForwardAddr = lib.mkOption {
       type = lib.types.str;
-      default = "127.0.0.1";
-      description = "Listen Address";
+      default = "100.100.100.100";
+      description = "Forward address for Tailscale DNS";
+    };
+
+    homeDomain = lib.mkOption {
+      type = lib.types.str;
+      default = "";
+      description = "Home domain to configure";
+    };
+
+    homeForwardAddr = lib.mkOption {
+      type = lib.types.str;
+      default = "192.168.1.1";
+      description = "Forward address for home DNS";
+    };
+
+    upstreamForwardAddr = lib.mkOption {
+      type = lib.types.str;
+      default = "1.1.1.1";
+      description = "Upstream DNS forward address";
+    };
+
+    listenAddress = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ "127.0.0.1" ];
+      description = "Addresses for unbound to listen on";
     };
   };
+
   config = {
     services.unbound = {
       enable = true;
-      settings =
-        let
-          domain = lib.mkIf (config.unbound.tsdomain != "") config.unbound.tsdomain;
-        in
-        {
-          domain-insecure = domain;
-          server = {
-            interface = [ config.unbound.listenAddress ];
-          };
-          forward-zone = [
-            (lib.mkIf (config.unbound.tsdomain != "") {
-              name = config.unbound.tsdomain;
-              forward-addr = "100.100.100.100";
-            })
+      settings = {
+        domain-insecure =
+          lib.optional (config.unbound.tsDomain != "") config.unbound.tsDomain
+          ++ lib.optional (config.unbound.homeDomain != "") config.unbound.homeDomain;
+
+        server = {
+          interface = config.unbound.listenAddress;
+        };
+
+        forward-zone =
+          lib.optional (config.unbound.tsDomain != "") {
+            name = config.unbound.tsDomain;
+            forward-addr = config.unbound.tsForwardAddr;
+          }
+          ++ lib.optional (config.unbound.homeDomain != "") {
+            name = config.unbound.homeDomain;
+            forward-addr = config.unbound.homeForwardAddr;
+          }
+          ++ [
             {
               name = ".";
-              forward-addr = "1.1.1.1";
+              forward-addr = config.unbound.upstreamForwardAddr;
             }
           ];
-        };
+      };
     };
   };
 }
