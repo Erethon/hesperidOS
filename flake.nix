0dd93857381395f81a3fa61d58df8e81d8c60df0
Dionysis Grigoropoulos <dgrig@erethon.com>
2026-06-25 18:20:50 +0300

    flake: Use nixfmt instead of -rfc-style

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
       ...
     }@inputs:
     {
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt;
       devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
         buildInputs = with nixpkgs.legacyPackages.x86_64-linux; [
           just
