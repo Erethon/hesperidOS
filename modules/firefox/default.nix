c604f03b94405e198cd77c88a92893410ab70cbe
Dionysis Grigoropoulos <dgrig@erethon.com>
2026-09-02 02:37:54 +0300

    firefox: Fix typo

--- modules/firefox/default.nix
+++ modules/firefox/default.nix
@@ -43,8 +43,6 @@
           install_url = "https://addons.mozilla.org/firefox/downloads/file/4702886/karakeep-1.2.9.xpi";
           installation_mode = "force_installed";
         };
-          installation_mode = "force_installed";
-        };
       };
       Preferences = {
         "geo.enabled" = {
